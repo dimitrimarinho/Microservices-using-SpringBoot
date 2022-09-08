764b2a7ea49463886f4386582a33d6c3a6a8d059
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-09-08 16:42:45 -0300

    Adding Lombok and Slf4j (log.info())

--- department-service/src/main/java/com/dailycodebuffer/department/entity/Department.java
+++ department-service/src/main/java/com/dailycodebuffer/department/entity/Department.java
@@ -5,7 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Department {
 
     @Id
@@ -14,30 +21,5 @@ public class Department {
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
-    
-	public Long getDepartmentId() {
-		return departmentId;
-	}
-	public void setDepartmentId(Long departmentId) {
-		this.departmentId = departmentId;
-	}
-	public String getDepartmentName() {
-		return departmentName;
-	}
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-	public String getDepartmentAddress() {
-		return departmentAddress;
-	}
-	public void setDepartmentAddress(String departmentAddress) {
-		this.departmentAddress = departmentAddress;
-	}
-	public String getDepartmentCode() {
-		return departmentCode;
-	}
-	public void setDepartmentCode(String departmentCode) {
-		this.departmentCode = departmentCode;
-	}
-   
+    	
 }
